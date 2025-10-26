8e627830cc9066618c6970dd045cda84c17b90b1
Camila K <camilafkapp@gmail.com>
2025-10-25 23:38:58 -0300

    se corrigieron rutas

--- api/src/app.js
+++ api/src/app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv').config({ path: '../../.env' });
 
 const app = express();
 app.use(cors());
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('hello world')
 })
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
