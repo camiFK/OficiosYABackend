12794b02c54ee6161cf8c80be8330e5753cdbb44
gonzav104 <gvelazquez@alumnos.unsada.edu.ar>
2025-11-04 21:52:13 -0300

    actualizar modelos de base de datos

--- api/src/models/ImagenPrestador.js
+++ api/src/models/ImagenPrestador.js
@@ -3,15 +3,63 @@ const {DataTypes, Model} = require('sequelize');
 class ImagenPrestador extends Model {
     static init(sequelize) {
         super.init({
-            id_imagen_prestador: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-            id_prestador: DataTypes.INTEGER,
-            ruta_imagen: DataTypes.STRING,
-            descripcion: DataTypes.STRING,
-            fecha_subida: {type: DataTypes.DATE, defaultValue: DataTypes.NOW},
+            id_imagen_prestador: { 
+                type: DataTypes.INTEGER, 
+                primaryKey: true, 
+                autoIncrement: true 
+            },
+            id_prestador: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                validate: {
+                    notNull: {
+                        msg: 'El ID del prestador es requerido'
+                    },
+                    isInt: {
+                        msg: 'El ID del prestador debe ser un número entero'
+                    }
+                }
+            },
+            ruta_imagen: {
+                type: DataTypes.STRING(255),
+                allowNull: false,
+                validate: {
+                    notEmpty: {
+                        msg: 'La ruta de la imagen es requerida'
+                    },
+                    len: {
+                        args: [1, 255],
+                        msg: 'La ruta debe tener entre 1 y 255 caracteres'
+                    }
+                }
+            },
+            descripcion: {
+                type: DataTypes.TEXT,
+                allowNull: true,
+                validate: {
+                    len: {
+                        args: [0, 500],
+                        msg: 'La descripción no puede exceder 500 caracteres'
+                    }
+                }
+            },
+            fecha_subida: {
+                type: DataTypes.DATE, 
+                defaultValue: DataTypes.NOW,
+                allowNull: false
+            },
         }, {
             sequelize,
             tableName: 'imagen_prestador',
-            timestamps: false
+            timestamps: false,
+            indexes: [
+                {
+                    fields: ['id_prestador']
+                },
+                {
+                    fields: ['fecha_subida']
+                }
+            ]
         });
     }
 }
--- api/src/models/ImagenSolicitud.js
+++ api/src/models/ImagenSolicitud.js
@@ -3,15 +3,63 @@ const {DataTypes, Model} = require('sequelize');
 class ImagenSolicitud extends Model {
     static init(sequelize) {
         super.init({
-            id_imagen_solicitud: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-            id_solicitud: DataTypes.INTEGER,
-            ruta_imagen: DataTypes.STRING,
-            descripcion: DataTypes.STRING,
-            fecha_subida: DataTypes.DATE,
+            id_imagen_solicitud: { 
+                type: DataTypes.INTEGER, 
+                primaryKey: true, 
+                autoIncrement: true 
+            },
+            id_solicitud: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                validate: {
+                    notNull: {
+                        msg: 'El ID de la solicitud es requerido'
+                    },
+                    isInt: {
+                        msg: 'El ID de la solicitud debe ser un número entero'
+                    }
+                }
+            },
+            ruta_imagen: {
+                type: DataTypes.STRING(255),
+                allowNull: false,
+                validate: {
+                    notEmpty: {
+                        msg: 'La ruta de la imagen es requerida'
+                    },
+                    len: {
+                        args: [1, 255],
+                        msg: 'La ruta debe tener entre 1 y 255 caracteres'
+                    }
+                }
+            },
+            descripcion: {
+                type: DataTypes.TEXT,
+                allowNull: true,
+                validate: {
+                    len: {
+                        args: [0, 500],
+                        msg: 'La descripción no puede exceder 500 caracteres'
+                    }
+                }
+            },
+            fecha_subida: {
+                type: DataTypes.DATE,
+                defaultValue: DataTypes.NOW,
+                allowNull: false
+            },
         }, {
             sequelize,
             tableName: 'imagen_solicitud',
-            timestamps: false
+            timestamps: false,
+            indexes: [
+                {
+                    fields: ['id_solicitud']
+                },
+                {
+                    fields: ['fecha_subida']
+                }
+            ]
         });
     }
 }
