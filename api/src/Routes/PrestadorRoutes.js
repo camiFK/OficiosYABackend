dd06181d2e2f0f423760c2d10b4898f02d429207
gonzav104 <gvelazquez@alumnos.unsada.edu.ar>
2025-11-05 17:51:54 -0300

    Agregar autenticacion a endpoint de solicitudes de prestador

--- api/src/Routes/PrestadorRoutes.js
+++ api/src/Routes/PrestadorRoutes.js
@@ -7,10 +7,10 @@ const authMiddleware = require('../Middlewares/authMiddleware');
 // Rutas generales de prestadores
 router.get('/', controller.getAllPrestadores);
 router.get('/:id', controller.getPrestadorById);
-router.delete('/delete/:id', controller.deletePrestador);
-router.put('/update/:id', controller.updatePrestador);
-router.put('/:id/categorias', controller.updateCategorias);
-router.get('/:id/solicitudes', controller.getSolicitudesByPrestadorId);
+router.delete('/delete/:id', authMiddleware.verifyToken, controller.deletePrestador);
+router.put('/update/:id', authMiddleware.verifyToken, controller.updatePrestador);
+router.put('/:id/categorias', authMiddleware.verifyToken, controller.updateCategorias);
+router.get('/:id/solicitudes', authMiddleware.verifyToken, controller.getSolicitudesByPrestadorId);
 
 // Rutas de imágenes de prestadores
 router.post('/images', 
