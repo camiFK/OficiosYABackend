97dc55632c165bec9973fd4f1dbe734c3a28c7f2
gonzav104 <gvelazquez@alumnos.unsada.edu.ar>
2025-11-05 23:47:18 -0300

    Se ajustaron rutas de clientes, prestadores y de usuarios

--- api/src/Routes/ClienteRoutes.js
+++ api/src/Routes/ClienteRoutes.js
@@ -5,5 +5,7 @@ const controller = require('../Controllers/ClienteController');
 router.get('/:id/solicitudes', controller.getSolicitudesByClienteId);
 router.post('/:id/solicitudes', controller.createClienteSolicitud);
 router.get('/solicitudes/:id', controller.getSolicitudById);
+router.put('/solicitudes/:id', controller.updateSolicitud);
+router.put('/solicitudes/:id/cancel', controller.cancelSolicitud);
 
 module.exports = router;
--- api/src/Routes/PrestadorRoutes.js
+++ api/src/Routes/PrestadorRoutes.js
@@ -7,7 +7,6 @@ const authMiddleware = require('../Middlewares/authMiddleware');
 // Rutas generales de prestadores
 router.get('/', controller.getAllPrestadores);
 router.get('/:id', controller.getPrestadorById);
-router.delete('/delete/:id', authMiddleware.verifyToken, controller.deletePrestador);
 router.put('/update/:id', authMiddleware.verifyToken, controller.updatePrestador);
 router.put('/:id/categorias', authMiddleware.verifyToken, controller.updateCategorias);
 router.get('/:id/solicitudes', authMiddleware.verifyToken, controller.getSolicitudesByPrestadorId);
