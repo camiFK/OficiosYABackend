791fa17aef0f818d5a18bd26351ba7a5f3642a94
gonzav104 <gvelazquez@alumnos.unsada.edu.ar>
2025-11-12 03:00:55 -0300

    feat: implementar módulos completos de calificaciones y notificaciones

---
+++ api/src/Routes/CalificacionRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+const CalificacionController = require('../Controllers/CalificacionController');
+const { verifyToken } = require('../Middlewares/authMiddleware');
+const ValidationMiddleware = require('../Middlewares/validationMiddleware');
+const ErrorHandler = require('../Middlewares/errorHandler');
+
+// POST /api/calificaciones: Registra la calificación de un prestador
+router.post('/',
+    verifyToken,
+    ValidationMiddleware.validateRequired(['estrellas', 'id_solicitud', 'id_prestador']),
+    ErrorHandler.asyncHandler(CalificacionController.crearCalificacion)
+);
+
+// GET /api/calificaciones/prestador/:id: Devuelve todas las calificaciones de un prestador
+router.get('/prestador/:id',
+    ValidationMiddleware.validatePositiveInteger('id'),
+    ErrorHandler.asyncHandler(CalificacionController.getCalificacionesByPrestador)
+);
+
+module.exports = router;
