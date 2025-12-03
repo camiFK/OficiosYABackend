cfd118f01145c7f567e00bd35c8f0e3a81cf3acc
gonzav104 <gvelazquez@alumnos.unsada.edu.ar>
2025-12-03 17:09:36 -0300

    Agregar validaciones y tokens a rutas de calificacion, categoria, cliente, imagen, prestador, rol y usuario

--- api/src/Routes/ClienteRoutes.js
+++ api/src/Routes/ClienteRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../Controllers/ClienteController');
+const { verifyToken } = require('../Middlewares/authMiddleware');
 
-router.get('/:id/solicitudes', controller.getSolicitudesByClienteId);
-router.post('/:id/solicitudes', controller.createClienteSolicitud);
-router.get('/solicitudes/:id', controller.getSolicitudById);
-router.put('/solicitudes/:id', controller.updateSolicitud);
-router.put('/solicitudes/:id/cancel', controller.cancelSolicitud);
-router.get('/solicitudes/:id/prestadores',controller.obtenerPrestadoresPorLocalidad);
-router.get('/solicitudes/:id/prestadores-cercanos',controller.obtenerPrestadoresPorLocalidadCercana);
+router.get('/:id/solicitudes', verifyToken, controller.getSolicitudesByClienteId);
+router.post('/:id/solicitudes', verifyToken, controller.createClienteSolicitud);
+router.get('/solicitudes/:id', verifyToken, controller.getSolicitudById);
+router.put('/solicitudes/:id', verifyToken, controller.updateSolicitud);
+router.put('/solicitudes/:id/cancel', verifyToken, controller.cancelSolicitud);
+router.get('/solicitudes/:id/prestadores', verifyToken, controller.obtenerPrestadoresPorLocalidad);
+router.get('/solicitudes/:id/prestadores-cercanos', verifyToken, controller.obtenerPrestadoresPorLocalidadCercana);
 
 module.exports = router;
--- api/src/Routes/PrestadorRoutes.js
+++ api/src/Routes/PrestadorRoutes.js
@@ -9,11 +9,12 @@ const ErrorHandler = require('../Middlewares/errorHandler');
 
 // Rutas generales de prestadores
 router.get('/', controller.getAllPrestadores);
-router.get('/:id', controller.getPrestadorById);
+router.get('/:id', authMiddleware.verifyToken, controller.getPrestadorById);
 router.put('/update/:id', authMiddleware.verifyToken, controller.updatePrestador);
 router.put('/:id/categorias', authMiddleware.verifyToken, controller.updateCategorias);
 router.get('/:id/solicitudes', authMiddleware.verifyToken, controller.getSolicitudesByPrestadorId);
 router.get('/:id/promedio-calificaciones', 
+    authMiddleware.verifyToken,
     ValidationMiddleware.validatePositiveInteger('id'),
     ErrorHandler.asyncHandler(calificacionController.getPromedioCalificaciones)
 );
